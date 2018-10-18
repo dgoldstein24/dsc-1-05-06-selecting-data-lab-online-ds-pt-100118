a16061de6b5a110b3bb4544a1312bdbc6aa633ed
David Goldstein <dgoldstein24@Davids-MBP-2.fios-router.home>
2018-10-18 13:06:35 -0400

    Failing insert test

---
+++ insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Mercury", "gray", 0, .55, 'no');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Venus", "yellow", 0, .82, 'no');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Earth", "blue", 1, 1.00, 'no');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Mars", "red", 2, .11, 'no');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Jupiter", "orange", 53, 317.90, 'no');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Saturn", "hazel", 62, 95.19, 'yes');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Uranus", "light blue", 27, 14.54, 'yes');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Neptune", "dark blue", 14, 17.15, 'yes');
+INSERT INTO planets (name, color, num_of_moons, mass, rings) VALUES ("Pluto", "brown", 5, .003, 'no');
